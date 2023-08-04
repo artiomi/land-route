a2125bbc14c4fa913647731f9abd6acc10eab56b
Artiom Iurcovschi <artiom.08@gmail.com>
2023-08-04 10:08:12 +0300

    update json parser logic

--- src/main/java/my/assessment/util/CountriesResourceParser.java
+++ src/main/java/my/assessment/util/CountriesResourceParser.java
@@ -1,16 +1,16 @@
 package my.assessment.util;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import my.assessment.entity.Country;
-import my.assessment.entity.Country.CountryBuilder;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -33,13 +33,22 @@ public class CountriesResourceParser {
     log.info("Start loading countries from file:{}", countriesResource.getFilename());
     Map<String, Country> result = new HashMap<>();
     try {
-      JsonNode jsonNode = objectMapper.readTree(countriesResource.getContentAsByteArray());
-      Iterator<JsonNode> nodes = jsonNode.elements();
-      while (nodes.hasNext()) {
-        JsonNode node = nodes.next();
-        Country country = buildInstance(node);
+      List<CountryRecord> coutryRecords = objectMapper.readValue(
+          countriesResource.getContentAsByteArray(), new TypeReference<>() {
+          });
+      for (var record : coutryRecords) {
+        validate(record);
+        Country country = Country.builder()
+            .id(record.id)
+            .region(record.region)
+            .name(record.countryName)
+            .borders(record.borders)
+            .lat(record.latlng.get(0))
+            .lon(record.latlng.get(1))
+            .build();
         result.put(country.getId(), country);
       }
+
       log.info("Countries load complete. Loaded {} entries.", result.size());
     } catch (IOException e) {
       throw new RuntimeException(String.format("File [%s] parsing failed", countriesResource.getFilename()), e);
@@ -47,44 +56,45 @@ public class CountriesResourceParser {
     return result;
   }
 
-  private Country buildInstance(JsonNode node) throws IOException {
-    String id = node.get("cca3").textValue();
-    String name = node.get("name").get("common").textValue();
-    String region = node.get("region").textValue();
-    List<String> borders = objectMapper.readValue(node.get("borders").traverse(), new TypeReference<>() {
-    });
-    List<Double> latLng = objectMapper.readValue(node.get("latlng").traverse(), new TypeReference<>() {
-    });
-    validate(id, name, region, borders, latLng);
-    CountryBuilder countryBuilder = Country.builder()
-        .id(id)
-        .region(region)
-        .name(name)
-        .borders(borders)
-        .lat(latLng.get(0))
-        .lon(latLng.get(1));
-
-    return countryBuilder.build();
-
-  }
-
-  private void validate(String id, String name, String region, List<String> borders, List<Double> latLng) {
-    if (StringUtils.isBlank(id)) {
+  private void validate(CountryRecord record) {
+    if (StringUtils.isBlank(record.id)) {
       throw new IllegalArgumentException("id can't be blank");
     }
-    if (StringUtils.isBlank(name)) {
+    if (StringUtils.isBlank(record.countryName)) {
       throw new IllegalArgumentException("name can't be blank");
     }
-    if (StringUtils.isBlank(region)) {
+    if (StringUtils.isBlank(record.region)) {
       throw new IllegalArgumentException("region can't be null");
     }
-    if (borders == null) {
+    if (record.borders == null) {
       throw new IllegalArgumentException("borders can't be null");
     }
-    if (latLng == null || latLng.size() != 2) {
+    if (record.latlng == null || record.latlng.size() != 2) {
       throw new IllegalArgumentException("latlng should contain exactly 2 values.");
     }
   }
 
+  @Setter
+  @NoArgsConstructor
+  static class CountryRecord {
+
+    @JsonProperty("cca3")
+    private String id;
+    private String countryName;
+    @JsonProperty("region")
+    private String region;
+    @JsonProperty("latlng")
+    private List<Double> latlng;
+    @JsonProperty("borders")
+    private List<String> borders;
+
+    @JsonProperty("name")
+    public void populateName(Map<String, Object> name) {
+      if (name.get("common") != null) {
+        this.countryName = String.valueOf(name.get("common"));
+      }
+    }
+
+  }
 
 }
